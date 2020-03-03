9cfbe2a95ec0d71295c1d78f5e10e0fbb7b62216
gjustice <garrett.justice50@gmail.com>
2020-03-03 14:46:53 -0500

    Added enum "Category" as an attribute for the "node" class.

--- iot/src/main.py
+++ iot/src/main.py
@@ -2,6 +2,7 @@ import network
 from network import Network
 from network import node
 from node import Node
+from node import nodeCategory
 from node import nodeStatus
 from node import nodeMode
 
@@ -10,15 +11,17 @@ def displayNetwork(network):
         print(i.getName())
         print('------------------------')
         
+        print('Device type:', i.getCategory())
         print('Power:', i.getPower())
         print('Energy consumption rate:', i.getEnergyConsumption())
         print('Bandwidth:', i.getBandwidth())
         print('Node health:', i.getStatus())
         print('Node security:', i.getMode())
+        print()
         
-# Construct IoT devices (name, power, consumption rate, bandwidth, security status, security mode)
-device1 = Node('Laptop_1', 100, 2, 20.45, nodeStatus.VULNERABLE, nodeMode.PASSIVE)
-device2 = Node('Cell_phone_1', 98, 4, 13.30, nodeStatus.REPAIRED, nodeMode.PASSIVE)
+# Construct IoT devices (name, category, power, consumption rate, bandwidth, security status, security mode)
+device1 = Node('Laptop_1', nodeCategory.LAPTOP, 100, 2, 20.45, nodeStatus.VULNERABLE, nodeMode.PASSIVE)
+device2 = Node('Cell_phone_1', nodeCategory.SMARTPHONE, 98, 4, 13.30, nodeStatus.REPAIRED, nodeMode.PASSIVE)
 
 # Initialize BAN with previously-constructed IoT devices
 ban = [device1, device2]
--- iot/src/node.py
+++ iot/src/node.py
@@ -1,18 +1,34 @@
 from enum import Enum
 
+# Defines type of device in IoT network. Different types of devices can be assumed to contain
+# varying levels of sensitive data, which allows for the assigning of different security "weights"
+# depending on the device's classification. A less-sensitive device can more aggressively conserve
+# battery by neglecting to authenticate network traffic.
+class nodeCategory(Enum):
+    DESKTOP = 0
+    LAPTOP = 1
+    SMARTPHONE = 2
+    SMARTWATCH= 3
+    HOME_ASSISTANT = 4
+    HOME_UTILITY = 5
+    KITCHEN_APPLIANCE = 6
+
+# Defines health of node
 class nodeStatus(Enum):
     COMPROMISED = 0
     VULNERABLE = 1
     REPAIRED = 2
-    
+  
+# Defines whether device is in passive or active security mode 
 class nodeMode(Enum):
     PASSIVE = 0
     SECURE = 1
 
 class Node:
     # Constructor
-    def __init__(self, name, power, energyConsumption, bandwidth, status, mode):
+    def __init__(self, name, category, power, energyConsumption, bandwidth, status, mode):
         self.name = name
+        self.category= category
         self.power = power
         self.energyConsumption = energyConsumption
         self.bandwidth = bandwidth
@@ -22,6 +38,8 @@ class Node:
     # Methods
     def setName(self, name):
         self.name = name
+    def setType(self, category):
+        self.category = category
     def setPower(self, power):
         self.power = power
     def setEnergyConsumption(self, energyConsumption):
@@ -36,6 +54,8 @@ class Node:
     # Accessors
     def getName(self):
         return self.name
+    def getCategory(self):
+        return self.category
     def getPower(self):
         return self.power
     def getEnergyConsumption(self):
