d5a1b52dd74fbb3f4ca5b8fbdf7a5ae85bd768b1
gjustice <garrett.justice50@gmail.com>
2020-04-19 15:17:10 -0400

    Can now put known Nodes and unidentifed Entities within BAN container

--- iot/src/main.py
+++ iot/src/main.py
@@ -9,15 +9,25 @@ from entity import nodeNature
 
 def displayNetwork(network):
     for i in network.ban:
-        print(i.getName())
-        print('------------------------')
+        if (i.__class__.__name__ == 'Node'):
+            print('Device class:', i.__class__.__name__)
+            print('Device name:', i.getName())
+            print('------------------------')
         
-        print('Power:', i.getPower())
-        print('Energy consumption rate:', i.getEnergyConsumption())
-        print('Energy rating:', i.getEnergyRating())
-        print('Data sensitivity:', i.getDataSensitivity())
-        print('Node security policy:', i.getMode())
-        print()
+            print('Power:', i.getPower())
+            print('Energy consumption rate:', i.getEnergyConsumption())
+            print('Energy rating:', i.getEnergyRating())
+            print('Data sensitivity:', i.getDataSensitivity())
+            print('Node security policy:', i.getMode())
+            print()
+        elif (i.__class__.__name__ == 'Entity'):
+            print('Device class:', i.__class__.__name__)
+            print('Device name:', i.getName())
+            print('------------------------')
+            
+            print('Nature:', i.getNature())
+            print('Estimated utility:', i.getUtility())
+        
         
 # Construct IoT devices (name, power, consumption rate, hasIdentifiables,
 # hasPasswords,hasBiometrics, hasTelemetry, hasMiscellaneous, security mode)
@@ -27,7 +37,7 @@ device2 = Node('Cell_phone_1', .9800, 2, False, True, False, True, False, nodeMo
 entity1 = Entity('CISCO Router', nodeNature.SELFISH, 0.9)
 
 # Initialize BAN with previously-constructed IoT devices
-ban = [device1, device2,]
+ban = [device1, device2, entity1]
 
 # Construct network
 network = Network(ban, True)
